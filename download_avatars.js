f7f387150aee9dedb479a5f2adf63b575851b721
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-14 01:37:36 +0000

    added a URL getter that returns a valid url and takes a username and a repo

--- download_avatars.js
+++ download_avatars.js
@@ -2,17 +2,24 @@
 let request = require('request');
 let fs = require('fs');
 
+const GITHUB_API_KEY = '153e7349fec2a30b41621ded8138bacc9d3e6372';
+const GITHUB_USER = 'THISS';
+const BASE_URL = `https://${GITHUB_USER}:${GITHUB_API_KEY}@api.github.com/`;
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 // compile our URL endpoint
-
+function getContributorsURL(repoOwner, repoName) {
+  return `${BASE_URL}repos/${repoOwner}/${repoName}/contributors`;
+}
+console.log(getContributorsURL('jquery', 'jquery'));
 // GET the URL
 // Parse the returned JSON
 // Loop Over the avatars
 // // Save each to disk under avatars/ directory
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  let url = getContributorsURL(repoOwner, repoName);
 }
 
 // Call our function
