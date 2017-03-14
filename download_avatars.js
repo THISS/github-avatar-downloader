9c5426f114d85a37276665c7c4abe328f01e89df
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-14 01:06:35 +0000

    importing two modules I will need, to download an image and save to disk. logged a welcome message

---
+++ download_avatars.js
@@ -0,0 +1,12 @@
+// import the request module and fs module
+let request = require('request');
+let fs = require('fs');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+// compile our URL endpoint
+
+// GET the URL
+// Parse the returned JSON
+// Loop Over the avatars
+// // Save each to disk under avatars/ directory
