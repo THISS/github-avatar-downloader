44e63313aad1c6419689388e33a8d0a672e04c68
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-14 02:18:06 +0000

    printing the avatars to console

--- download_avatars.js
+++ download_avatars.js
@@ -12,35 +12,43 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getContributorsURL(repoOwner, repoName) {
   return `${BASE_URL}repos/${repoOwner}/${repoName}/contributors`;
 }
-// GET the URL
-function getContributorsJSON(url) {
+
+// Helper function to print each contributor Avatar to console
+function printAvatarURL(contributor) {
+  console.log(contributor.avatar_url);
+}
+
+// Get Contributors
+// Our main controlling function
+function getRepoContributors(repoOwner, repoName, cb) {
+  let url = getContributorsURL(repoOwner, repoName);
+
   let options = {
     url: url,
     headers: {
       'User-Agent': 'GitHub Avatar Downloader - Student Project'
     }
   };
+
+  //  Main request to GitHub api
   request.get(options, (err, response, body) => {
-    return JSON.parse(body);
-  })
-  .on('response', (res) =>{
-    console.log(`Response Code: ${res.statusCode}`);
-    console.log(`Response Message: ${res.statusMessage}`);
-    });
+    // Parse the returned JSON and pass it to our callback
+    cb(null, JSON.parse(body));
+  });
+  // .on('response', (res) =>{
+  //   console.log(`Response Code: ${res.statusCode}`);
+  //   console.log(`Response Message: ${res.statusMessage}`);
+  // });
 }
-let url = getContributorsURL('nodejs', 'node');
-let contributorsList = getContributorsJSON(url);
 
-// Parse the returned JSON
-// Loop Over the avatars
-// // Save each to disk under avatars/ directory
-
-function getRepoContributors(repoOwner, repoName, cb) {
-  let url = getContributorsURL(repoOwner, repoName);
-}
 
 // Call our function
 getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  // Loop Over the avatars
+  result.forEach(printAvatarURL);
 });
+
+
+// // Save each to disk under avatars/ directory
