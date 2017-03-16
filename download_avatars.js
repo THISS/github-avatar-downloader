5715a7586f03b22bbfe3beeada8eb15e209196fe
Brenton Holswich <brenton@Brentons-MacBook-Pro.local>
2017-03-16 08:29:05 -0700

    wrapped all code in a function. Called the func

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,20 @@
 // Adding .env key vals to environment
 require('dotenv').config();
-const gitLib = require('./github-lib.js');
-// Wrap everything in a try catch block to make sure we handle the errors nicely
-try {
+// Get Our GitHub Library
+const gitLib = require('./github-lib');
+
+function main() {
   // Initial Setup - Make Sure Process.env has our two keys from our .env file
   if(!((process.env.GITHUB_API && process.env.GITHUB_API.length > 0) && (process.env.USER && process.env.USER.length > 0))) {
     throw(new Error("Need to configure a .env file in: " + __dirname));
   }
-  // Get Our GitHub Library
-  let gitLib = require('./github-lib');
 
   // Makes sure we are only receiving two words as input
   if(!(process.argv.length < 5 && process.argv[3])) {
     console.log("Please only provide a username and a repository in the form <USER> <REPOSITORY>");
     throw(new Error("Please Enter two words - no more - no less"));
   }
+
   // Get the User Input
   const REPO = process.argv[3];
   const OWNER = process.argv[2];
@@ -29,6 +29,11 @@ try {
     // Loop Over the avatars and download them
     result.forEach(gitLib.dataGrabber);
   });
+}
+
+// Wrap everything in a try catch block to make sure we handle the errors nicely
+try {
+  main();
 }catch(e) {
   console.log(`An Error Occurred: ${e.name} - "${e.message}"`);
 }
